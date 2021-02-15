5c5e73126fe6e2457470382868c04397d3a2c964
cmarshall831 <connormarshall343@gmail.com>
2021-02-14 21:26:08 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+* lab.js
+* Author: Connor Marshall <cmarshall@csumb.edu>
+* Credit: Wes Modes <wmodes@csumb.edu>
+* Created: February 2021
+* Liscence: Public Domain
+/*
+
+//Variables for make, model, color, and year of vehicle
+
+var make = "Ford";
+var make = "Fusion";
+var color = "White";
+var year = 2011;
+
+//Variable for whether you own it or not
+var age = 2021 - year;
+
+//Output each variable using document.writeln()
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
